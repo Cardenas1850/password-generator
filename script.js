cd97099d69b19963d7ec3ee61ba5b84b5c181ba9
Jason <cardenas7122@gmail.com>
2022-03-20 20:35:25 -0400

    upadte script.js

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 // Assignment code here
-var begin;
-var confirmLowerCase;
-var confirmUpperCase;
-var confirmSymbol;
-var confirmInteger;
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-const lowerCase=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-const confirmUpperCase=[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
-const symbol=["1","@","#","$","%","^","&","(",")","?","<",">"];
-const interger=[1,2,3,4,5,6,7,8,9,0];
+// Get references to the #generate element
+var lowerCase=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+var UpperCase=[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+var symbol=["1","@","#","$","%","^","&","(",")","?","<",">"];
+var interger=[1,2,3,4,5,6,7,8,9,0];
+
+
+function passwordSetup() {
+  var isValid = false;
+  do {
+    var totalCharacters = prompt("Select a whole number between 8 and 128 characters.");
+    var confirmNumbers = confirm("Confirm if you would like numbers in your password.");
+    var confirmLowerCase = confirm("Confirm if you would like lowercase letters in your password.");
+    var confirmUpperCase  = confirm("Confirm if you would like uppercase letter in your password");
+    var confrimSymbol = confirm("Confirm if you would like special symbols in your password.");
+    var selections = {
+      totalCharacters: totalCharacters,
+      confirmNumbers: confirmNumbers,
+      confirmLowerCase: confirmLowerCase,
+      confirmUpperCase: confirmUpperCase,
+      confrimSymbol: confrimSymbol
+    }
+
+  if((totalCharacters < 8)||(totalCharacters > 128))
+  window.alert("You must choose a number between 8 and 128");
+  else if ((!confirmNumbers)&&(!confirmLowerCase)&&(!confirmUpperCase)&&(confrimSymbol))
+  window.alert("Your password must contain at least one charcter option");
+  else 
+  isValid=true;
+
+} while(!isValid);
+return selections;
+}
 
 // Write password to the #password input
 function writePassword() {
